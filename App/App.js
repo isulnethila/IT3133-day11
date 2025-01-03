728fe8779820789bd517f3f353f0487725927ceb
isulnethila <139636196+isulnethila@users.noreply.github.com>
2025-01-03 16:16:36 +0530

    update app.js

--- App/App.js
+++ App/App.js
@@ -1,20 +1,30 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+import Home from './Components/Home';
+import ContactUs from './Components/ContactUs';
+import AboutUs from './Components/AboutUs';
 
+const Stack = createNativeStackNavigator();
 export default function App() {
+  const stack = createNativeStackNavigator();
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaProvider>
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName='home'
+          screenOptions={{
+            headerStyle: { backgroundColor: '#e2bee2' },
+          }}
+        >
+          <Stack.Screen name='home' component={Home} options={{ title: "Welcome" }} />
+          <Stack.Screen name='contact' component={ContactUs} options={{ title: "Contact Us" }} />
+          <Stack.Screen name='aboutus' component={AboutUs} options={{ title: "About Us" }} />
+
+        </Stack.Navigator>
+
+      </NavigationContainer>
+    </SafeAreaProvider>
+
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
